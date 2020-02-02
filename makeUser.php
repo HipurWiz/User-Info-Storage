5d55b6a1fd04fed6ee61a22990d9bc43cc2d5c73
CCjump21 <32403618+chutianchen@users.noreply.github.com>
2020-02-02 10:44:39 -0500

    Create makeUser.php

---
+++ makeUser.php
@@ -0,0 +1,13 @@
+<?php
+// Make sure that you run this file with the form included with the file on localhost or your own domain. This script stores a users points starting at 0
+if (isset($_POST["submit"]))
+	$txt = '.txt';
+	$message = $_POST['username'];
+	$File = $message .= ".txt";
+	$Handle = fopen($File, 'w');
+	$Data = 0;
+	fwrite($Handle, $Data);
+	fclose($Handle);
+	print("You were signed up as ");
+	print($_POST['subject']);
+?>
